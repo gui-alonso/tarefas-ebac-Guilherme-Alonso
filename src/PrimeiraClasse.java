b9ebe32e7fffb4caa66b7863ec5fbe609225a8f7
Guilherme Alonso <alonsogui011@gmail.com>
2022-12-19 13:45:56 -0300

    tarefas-ebac-Guilherme-Alonso

---
+++ src/PrimeiraClasse.java
@@ -0,0 +1,7 @@
+public class PrimeiraClasse {
+
+    public static void main(String args[]) {
+        String name = "Guilherme";
+        System.out.println("Hello! Meu nome é "+name+ "!");
+    }
+}
